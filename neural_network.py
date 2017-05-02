8845efae328931fb971a343e55a375e1c08d0f86
marcosrabaioli <eng.marcosra@gmail.com>
2017-05-02 11:48:18 -0300

    Adicionados arquivos de projeto
    Adicionada função initialize_networks que inicializa a rede neural

---
+++ neural_network.py
@@ -0,0 +1,10 @@
+import random
+
+def initialize_network(n_inputs, n_hidden, n_outputs):
+
+    network = list()
+    hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
