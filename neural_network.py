d7c773205631d11a93827140a11f2efa597366be
marcosrabaioli <eng.marcosra@gmail.com>
2017-05-03 15:30:25 -0300

    Rede neural finalizada
    Rede neural de uma camada escondida finalizada

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
-from random import random
+from random import random, randrange
 from random import seed
 from math import exp
+from csv import reader
 
 # Initialize a network
 # n_inputs: numero de entradas
@@ -101,7 +102,7 @@ def train_network(network, train, l_rate, n_epoch, n_outputs):
             sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
             backward_propagate_error(network, expected)
             update_weights(network, row, l_rate)
-        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+        #print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
 
 
 # Make a prediction with a network
@@ -109,35 +110,102 @@ def predict(network, row):
     outputs = forward_propagate(network, row)
     return outputs.index(max(outputs))
 
-seed(1)
+
+
+# seed(1)
+#
+#
+#
+#
+#
+# # Conjunto de dados para treinar a rede
+# dataset = [[2.7810836,2.550537003,0],
+#            [1.465489372,2.362125076,0],
+#            [3.396561688,4.400293529,0],
+#            [1.38807019,1.850220317,0],
+#            [3.06407232,3.005305973,0],
+#            [7.627531214,2.759262235,1],
+#            [5.332441248,2.088626775,1],
+#            [6.922596716,1.77106367,1],
+#            [8.675418651,-0.242068655,1],
+#            [7.673756466,3.508563011,1]]
+# # calcula tamanho da entrada
+# n_inputs = len(dataset[0]) - 1
+# # calcula quantas classes de saida existem
+# n_outputs = len(set([row[-1] for row in dataset]))
+# # Inicializa a rede
+# network = initialize_network(n_inputs, 2, n_outputs)
+# # treina a rede
+# train_network(network, dataset, 0.5, 20, n_outputs)
+# for layer in network:
+#     print(layer)
+#
+# for row in dataset:
+#     prediction = predict(network, row)
+#     print('Expected=%d, Got=%d' % (row[-1], prediction))
+
+def load_csv(filename):
+
+    dataset = []
+    file = open(filename, 'r')
+    csv_reader = reader(file)
+
+    for row in csv_reader:
+        dataset.append([float(col) for col in row[0].split('\t')])
+        dataset[-1][-1] = int(dataset[-1][-1])
+
+    return dataset
+
+
+def dataset_minmax(dataset):
+
+    minmax = [[min(col), max(col)] for col in zip(*dataset)]
+    return minmax
+
+def normalize_dataset(dataset):
+
+    minmax = dataset_minmax(dataset)
+    dataset_normalize = []
+
+    for row in dataset:
+        row_normalize = [(row[col] - minmax[col][0])/(minmax[col][1] - minmax[col][0]) for col in range(0, len(row)-1)]
+        row_normalize.append(row[-1])
+        dataset_normalize.append(row_normalize)
+
+    return dataset_normalize
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+dataset = load_csv('clound.csv')
+dataset = normalize_dataset(dataset)
+dataset_split = cross_validation_split(dataset, 5)
+
+dataset_train = dataset_split[0]
 
 
 
-
-
-# Conjunto de dados para treinar a rede
-dataset = [[2.7810836,2.550537003,0],
-           [1.465489372,2.362125076,0],
-           [3.396561688,4.400293529,0],
-           [1.38807019,1.850220317,0],
-           [3.06407232,3.005305973,0],
-           [7.627531214,2.759262235,1],
-           [5.332441248,2.088626775,1],
-           [6.922596716,1.77106367,1],
-           [8.675418651,-0.242068655,1],
-           [7.673756466,3.508563011,1]]
-# calcula tamanho da entrada
-n_inputs = len(dataset[0]) - 1
-# calcula quantas classes de saida existem
+n_inputs = len(dataset_train[0])-1
 n_outputs = len(set([row[-1] for row in dataset]))
-# Inicializa a rede
-network = initialize_network(n_inputs, 2, n_outputs)
-# treina a rede
-train_network(network, dataset, 0.5, 20, n_outputs)
-for layer in network:
-    print(layer)
+network = initialize_network(n_inputs, 10, n_outputs)
 
+train_network(network, dataset_train, 0.1, 1000, n_outputs)
+
+ok = 0
 for row in dataset:
     prediction = predict(network, row)
-    print('Expected=%d, Got=%d' % (row[-1], prediction))
+    if row[-1] == prediction:
+        ok += 1
+
+print('Mean Accuracy: ', (ok/len(dataset))*100)
 
